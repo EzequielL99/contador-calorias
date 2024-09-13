3b76b49bf59d906a8f53d56c6c8fe56d96119a10
EzequielLopez <ezequiel.lopez.vallejos@gmail.com>
2024-09-13 17:36:01 -0300

    Se implementa useReducer para contener las actividades

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,23 +1,57 @@
-import { useState, ChangeEvent } from "react";
+import { useState, ChangeEvent, FormEvent, Dispatch } from "react";
+import { v4 as uuidv4 } from 'uuid';
+
 import { categories } from "../data/categories";
+import type { Activity } from "../types";
+import { ActivityActions } from "../reducers/activity-reducer";
 
-export default function Form() {
+type FormProps = {
+    dispatch: Dispatch<ActivityActions>
+}
 
-    const [activity, setActivity] = useState({
-        category: 1,
-        name: '',
-        calories: 0
-    });
+const initialState : Activity = {
+    id: uuidv4(),
+    category: 1,
+    name: '',
+    calories: 0
+}
+
+export default function Form({ dispatch }: FormProps) {
+
+    const [activity, setActivity] = useState<Activity>(initialState);
 
     const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
+        const isNumberField = ['category', 'calories'].includes(e.target.id);
+
         setActivity({
             ...activity,
-            [e.target.id]: e.target.value
+            [e.target.id]: isNumberField ? +e.target.value : e.target.value
         })
     }
 
+    const isValidActivity = () => {
+        const { name, calories } = activity;
+
+        return name.trim() !== '' && calories > 0
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        dispatch({ type: 'save-activity', payload: { newActivity: activity } });
+
+        setActivity({
+            ...initialState,
+            id: uuidv4()
+        });
+    }
+
     return (
-        <form action="" className="space-y-5 bg-white shadow p-10 rounded-lg">
+        <form
+            action=""
+            className="space-y-5 bg-white shadow p-10 rounded-lg"
+            onSubmit={handleSubmit}
+        >
             <div className="grid grid-cols-1 gap-3">
                 <label htmlFor="category" className="font-bold">Categoria:</label>
                 <select
@@ -60,10 +94,12 @@ export default function Form() {
                 />
             </div>
 
-            <input 
-                type="submit" 
-                className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
+            <input
+                type="submit"
+                className="bg-gray-800 hover:bg-gray-900 w-full p-2 
+                font-bold uppercase text-white cursor-pointer disabled:opacity-10"
                 value="Guardar"
+                disabled={!isValidActivity()}
             />
         </form>
     )
---
+++ src/reducers/activity-reducer.ts
@@ -0,0 +1,31 @@
+import { Activity } from "../types"
+
+export type ActivityActions = {
+    type: 'save-activity', payload: { newActivity: Activity }
+}
+
+type ActivityState = {
+    activities: Activity[]
+}
+
+export const initialState: ActivityState = {
+    activities: []
+}
+
+export const activityReducer = (
+    state: ActivityState = initialState,
+    action: ActivityActions
+) => {
+
+    if (action.type === 'save-activity') {
+        // Este codigo maneja la lógica para actualizar el state
+        console.log(action.payload.newActivity);
+
+        return {
+            ...state,
+            activities: [...state.activities, action.payload.newActivity]
+        }
+    }
+
+    return state;
+}
